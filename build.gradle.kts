b947df8fe267173eb202dbaf50b5c6d5ecd52999
yamee-dev <ryo.eiyama@icloud.com>
2019-11-08 16:10:25 +0900

    add: commit忘れファイル

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.2.1.RELEASE"
+	id("io.spring.dependency-management") version "1.0.8.RELEASE"
+	kotlin("jvm") version "1.3.50"
+	kotlin("plugin.spring") version "1.3.50"
+	kotlin("plugin.jpa") version "1.3.50"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("mysql:mysql-connector-java")
+	testCompile("org.springframework.boot:spring-boot-starter-test")
+	testCompile("org.assertj:assertj-core:3.8.0")
+
+//	testCompile("org.assertj:assertj-core:3.8.0")
+//	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+//		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+//	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
---
+++ src/main/kotlin/com/example/kotlin_restapi/KotlinRestapiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_restapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRestapiApplication>(*args)
+}
