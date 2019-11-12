7f9279436b043c794ec02d0f32661dbd2c472343
yamee-dev <ryo.eiyama@icloud.com>
2019-11-12 10:39:07 +0900

    fix: configをjava→ktに変更

---
+++ src/main/kotlin/com/example/kotlin_restapi/config/WebMvcConfig.kt
@@ -0,0 +1,28 @@
+package com.example.kotlin_restapi.config
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+
+    @Bean
+    fun corsFilter(): FilterRegistrationBean<CorsFilter> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.addAllowedOrigin(CorsConfiguration.ALL)
+        config.addAllowedHeader(CorsConfiguration.ALL)
+        config.addAllowedMethod(CorsConfiguration.ALL)
+        source.registerCorsConfiguration("/**", config)
+        val bean = FilterRegistrationBean(CorsFilter(source))
+        bean.order = Ordered.HIGHEST_PRECEDENCE
+        return bean
+    }
+}
