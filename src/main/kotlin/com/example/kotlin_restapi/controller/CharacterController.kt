8b67e1dc3353d9904bb2954e8159651a0347e4ad
yamee-dev <ryo.eiyama@icloud.com>
2019-11-13 10:22:45 +0900

    fix: CORSの制御をController側に移動

--- src/main/kotlin/com/example/kotlin_restapi/controller/CharacterController.kt
+++ src/main/kotlin/com/example/kotlin_restapi/controller/CharacterController.kt
@@ -7,9 +7,9 @@ import java.util.*
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 @RequestMapping("/api/v1/characters")
+@CrossOrigin("*")
 class CharacterController (private val characterService: CharacterService){
 
     @GetMapping("")
